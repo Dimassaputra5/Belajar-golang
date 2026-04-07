a770accc626495552f8ae4da1cca15c309b152e9
Muhamad Dimas Saputra <dimassaputrq96@gmail.com>
2026-04-07 22:09:46 +0700

    update

--- brute.go
+++ brute.go
@@ -3,12 +3,17 @@ package main
 import (
 	"bruteforce/function"
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Menjalankan Brute Force 4 karakter ASCII Extend(8 bit)")
 	target := function.RandomPassword()
 	fmt.Printf("Password yang akan di brute force adalah: %s\n", string(target[:]))
+	waktuMulai := time.Now()
 	result, count := function.Force(target[:])
+	waktuEksekusi := time.Since(waktuMulai)
 	fmt.Printf("Hasil brute force: %s dengan %s percobaan\n", result, function.HundredFormat(count))
+
+	fmt.Printf("Waktu yang dibutuhkan: %s\n", waktuEksekusi)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bruteforce
 
 go 1.25.0
+
+require golang.org/x/text v0.35.0
